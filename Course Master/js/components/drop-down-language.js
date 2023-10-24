6e179dccd110a7cb601febccca7b91064f59d57a
eugene4217 <eugenie.kozich@gmail.com>
2023-10-24 18:01:21 +0300

    day

--- Course Master/js/components/drop-down-language.js
+++ Course Master/js/components/drop-down-language.js
@@ -1,31 +1,17 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-    window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-          let openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-    openDropdown.classList.remove('show');
-    }
-     }
-    }
+  }
 }
 
-function myFunction() {
-  document.getElementById("myDropdown-coment").classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn-coment')) {
-      let dropdowns = document.getElementsByClassName("dropdown-content-coment");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-  openDropdown.classList.remove('show');
-  }
-   }
-  }
-}
+
